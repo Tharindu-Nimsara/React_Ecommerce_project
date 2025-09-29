1bc8708a33179000384132f0dd800f0090005c5c
Tharindu <tharinduonline1080@gmail.com>
2025-09-29 23:35:08 +0530

    convert home and checkout to async await

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,9 +8,13 @@ export default function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
-      setProducts(response.data);
-    });
+    const getHomeData = async () => {
+        const response = await axios.get("/api/products");
+        setProducts(response.data);
+    }
+    
+    getHomeData();
+   
   }, []); //empty dependence array means this will run only once
 
   return (
